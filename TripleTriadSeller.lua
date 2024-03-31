03b82283ba0bed979c2a431c31e64206b2f58604
UcanPatates <56081921+UcanPatatess@users.noreply.github.com>
2024-03-31 19:49:59 +0300

    Update TripleTriadSeller.lua
    Better for users "thanks ice"

--- TripleTriadSeller.lua
+++ TripleTriadSeller.lua
@@ -1,7 +1,59 @@
-yield("/target Triple Triad Trader")
-yield("/wait 0.4")
-yield("/pinteract")
-yield("/wait 0.2")
+--[[
+
+      ***********************************************
+      *          Triple Triad Seller                * 
+      *  Sells your acumulated Triple Triad cards   *
+      ***********************************************
+
+      **********************
+      * Version  |  0.0.2  *
+      **********************
+
+      -> 0.0.1  : +Added Teleport and auto walk to Triple Triad Seller
+      -> 0.0.1  : Just the seller
+
+      ***************
+      * Description *
+      ***************
+
+      This script basicly just sells all of your acumulated Cards with one click
+      You can make a macro with /snd run "Your_script_name_here"
+
+      *********************
+      *  Required Plugins *
+      *********************
+
+
+      Plugins that are used are:
+      -> vnavmesh : https://puni.sh/api/repository/veyn
+      -> Something Need Doing [Expanded Edition] : https://puni.sh/api/repository/croizat
+           
+]]
+
+--[[
+
+  ************
+  *  Script  *
+  *   Start  *
+  ************
+
+]]
+function ZoneTransition()
+    repeat 
+        yield("/wait 0.5")
+    until not IsPlayerAvailable()
+    repeat 
+        yield("/wait 0.5")
+    until IsPlayerAvailable()
+end
+
+function TargetedInteract(target)
+    yield("/target "..target.." <wait.0.5>")
+    yield("/pinteract <wait.1>")
+end
+
+function TripleSeller()
+
 yield("/pcall SelectIconString false 1")
 yield("/wait 0.4")
 repeat
@@ -33,3 +85,17 @@ else
     end
   end
 end
+end
+
+function WalkTo(x, y, z)
+    PathfindAndMoveTo(x, y, z, false)
+    while (PathIsRunning() or PathfindInProgress()) do
+        yield("/wait 0.5")
+    end
+end
+
+yield("/tp The Gold Saucer")
+ZoneTransition()
+WalkTo(-55,1,16)
+TargetedInteract("Triple Triad Trader")
+TripleSeller()
