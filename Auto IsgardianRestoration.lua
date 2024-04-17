71dc0afecb9c58b8017cb8c4a3b3e9fe10b0db80
UcanPatates <56081921+UcanPatatess@users.noreply.github.com>
2024-04-17 23:32:37 +0300

    Update Auto IsgardianRestoration.lua

--- Auto IsgardianRestoration.lua
+++ Auto IsgardianRestoration.lua
@@ -7,9 +7,10 @@
     Author: UcanPatates  
 
     **********************
-    * Version  |  0.0.1  *
+    * Version  |  0.0.2  *
     **********************
 
+    -> 0.0.2  : Added artisan automation
     -> 0.0.1  : This will trade in your collectable grade 4 rope
 
 
@@ -18,6 +19,7 @@
     ***************
 
     This script will trade in your ropes to Potkin if your inventory is full or if you open the trade in screen
+    if you are using artisan you need to put your list id in settings
 
     *********************
     *  Required Plugins *
@@ -31,13 +33,28 @@
     -> Something Need Doing [Expanded Edition] : https://puni.sh/api/repository/croizat
           -> If there's extra settings needed to be changed in the plugin
           -> I try and put it below it so people know that it's related to this
+    -> Artisan : https://love.puni.sh/ment.json
+    **************
+    *  SETTINGS  *
+    **************
+]]
 
+
+
+ArtisanListID = "12017" --Your artisan list
+
+TurnInWait = 1 --default is 1 you can change it to 0.5 or something lower too
+
+--[[
     ************
     *  Script  *
     *   Start  *
     ************
 
 ]]
+
+
+
 function AmIThere(zoneid)
     if GetZoneID() == zoneid then
         return true
@@ -78,12 +95,12 @@ while true do
     end
     while AmIThere(886) and IsIsgardianOpen() do
         yield("/pcall HWDSupply true 1 5")
-        yield("/wait 1")
+        yield("/wait "..TurnInWait)
         if GetNodeText("_TextError",1) == "You do not possess the requested item." and IsAddonVisible("_TextError") then
             yield("/pcall HWDSupply true -1")
         end
     end
-    --if GetInventoryFreeSlotCount() ~= 0 then
-        --this was Planned to resume artisan
-    --end
+    if GetInventoryFreeSlotCount() ~= 0 and IsIsgardianOpen()==false and GetCharacterCondition(5)==false then
+        yield("/artisan lists "..ArtisanListID.." start")
+    end
 end
